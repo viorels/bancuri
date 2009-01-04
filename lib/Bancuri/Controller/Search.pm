9e9b138bdc571429dcf737ca8c7c50d809808406
Viorel Stirbu <viorels@gmail.com>
2009-01-04 23:51:14 +0200

    Reparat get in loc de post la search Inlocuit current_version cu current inca intr-un loc

--- lib/Bancuri/Controller/Search.pm
+++ lib/Bancuri/Controller/Search.pm
@@ -71,7 +71,7 @@ sub results : Private {
     if (@ids) {
         $jokes = $c->model('BancuriDB::Joke')->search_ids(\@ids);
         
-        my @joke_texts = map { $_->version->text } @$jokes;
+        my @joke_texts = map { $_->current->text } @$jokes;
         my $joke_snippets = $c->model('Xapian')->highlight(\@joke_texts, $keywords);
         
         for (my $i=0; $i<@$jokes; $i++) {
