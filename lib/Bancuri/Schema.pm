04e05cbf3317573d8c90b6e6a2a92063a38d2838
Viorel Stirbu <vio@vio-laptop>
2009-02-15 05:21:46 +0200

    Mutat ResultSet in Schema

--- lib/Bancuri/Schema.pm
+++ lib/Bancuri/Schema.pm
@@ -5,11 +5,14 @@ use warnings;
 
 use base 'DBIx::Class::Schema';
 
-__PACKAGE__->load_classes;
+__PACKAGE__->load_namespaces(
+    result_namespace => '+Bancuri::Schema',
+    resultset_namespace => 'ResultSet',
+);
 
 
-# Created by DBIx::Class::Schema::Loader v0.04005 @ 2009-01-11 03:51:59
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:wXiOjJqc6bQdY09S+34P5w
+# Created by DBIx::Class::Schema::Loader v0.04005 @ 2009-02-15 05:15:37
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:gGdRSHgzFFa23QR89CyEvA
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
--- script/bancuri_update_schema.pl
+++ script/bancuri_update_schema.pl
@@ -6,13 +6,17 @@ use warnings;
 my $constraint = $ARGV[0] || '.*';
 $constraint = qr/$constraint/;
 
+our $schema_class = "Bancuri::Schema";
+
 use FindBin;
 use DBIx::Class::Schema::Loader qw| make_schema_at |;
 make_schema_at(
-    "Bancuri::Schema",
+    $schema_class,
     {
         debug          => 1,
-        use_namespaces => 0,
+        use_namespaces => 1,
+        result_namespace => "+$schema_class",
+        resultset_namespace => 'ResultSet',
         components     => [qw/InflateColumn::DateTime/],
         dump_directory => "$FindBin::Bin/../lib",
         constraint     => $constraint,
